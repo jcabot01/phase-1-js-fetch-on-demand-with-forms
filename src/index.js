4a06dcd5b8e78efd5e87952b2fed5c4cff3ec00c
JonCabot <jfc0053@gmail.com>
2021-12-13 21:22:56 -0600

    commit tree 5f3f00661be1391b2c13e6de227afaa353714be8

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form')
+ 
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault()
+      const input = document.querySelector('input#searchByID');
+      
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(res => res.json())
+      .then(json => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = json.title;
+        summary.innerText = json.summary;
+      })
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
